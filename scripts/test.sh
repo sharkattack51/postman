4699ff52a568da6139aca885e3aeb54c048c5ca7
Koichi Kasai <sharkattack51@gmail.com>
2024-10-08 14:09:57 +0900

    remove coverprofiles

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,6 +6,9 @@ if [ "$1" = "" ]; then
 elif [ "$1" = "-v" ]; then
 	go test -v ./... -count=1
 elif [ "$1" = "-c" ]; then
+	if [ ! -d ${dir}/../test ]; then
+		mkdir ${dir}/../test
+	fi
 	go test -cover ./... -coverprofile=${dir}/../test/cover.out
 	go tool cover -html=${dir}/../test/cover.out -o ${dir}/../test/cover.html
 fi
